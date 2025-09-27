cbe69a25827566039340ee8d6425e5319d2c104a
Ranvosky <53233079+Ranvosky@users.noreply.github.com>
2025-09-26 19:11:02 -0600

    Cambios sobre seccion de anuncios

--- private/js/index.js
+++ private/js/index.js
@@ -12,18 +12,19 @@ includeHTML("footer-container", "view/partials/footer.html");
 
 const acordeon = document.getElementsByClassName("contenedor");
 
-for (let i = 0; i < acordeon.length; i++) {
-    acordeon[i].addEventListener("click", function () {
+for (const item of acordeon) {
+    item.addEventListener("click", function () {
         if (this.classList.contains("activa")) {
             // If the clicked element already has "activa", remove it
             this.classList.remove("activa");
         } else {
             // Remove "activa" class from all elements
-            for (let j = 0; j < acordeon.length; j++) {
-                acordeon[j].classList.remove("activa");
-            }
+            Array.from(acordeon).forEach((el) => el.classList.remove("activa"));
             // Add "activa" class to the clicked element
             this.classList.add("activa");
+            this.scrollIntoView({ 
+                //behavior: "smooth", 
+                block: "center" });
         }
     });
 }
